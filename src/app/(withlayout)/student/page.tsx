ef25f42c6fd1e915f6b8ceb29e3542b40b5032e1
webnur <mdnuruli291@gmail.com>
2024-01-30 06:48:30 +0600

    setup project and layout. add and design

---
+++ src/app/(withlayout)/student/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Student = () => {
+  return <div style={{ color: "black" }}>student page</div>;
+};
+
+export default Student;
---
+++ src/app/(withlayout)/super-admin/page.tsx
@@ -0,0 +1,9 @@
+const SuparAdmin = () => {
+  return (
+    <div>
+      <h1>this page for super admin</h1>
+    </div>
+  );
+};
+
+export default SuparAdmin;
