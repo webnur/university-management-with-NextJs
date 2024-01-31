dda214c2283130b883bf6ae75137d974bdcd69db
webnur <mdnuruli291@gmail.com>
2024-02-01 05:55:36 +0600

    done sidebar items

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,46 +2,14 @@
 import { Layout, Menu } from "antd";
 import { useState } from "react";
 import type { MenuProps } from "antd";
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-const { Sider } = Layout;
-type MenuItem = Required<MenuProps>["items"][number];
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
 
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
+import { SidebarItems } from "@/constants/SidebarItems";
+import { USER_ROLE } from "@/constants/role";
+const { Sider } = Layout;
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const role = USER_ROLE.STUDENT;
   return (
     <Sider
       collapsible
@@ -72,7 +40,7 @@ const Sidebar = () => {
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
-        items={items}
+        items={SidebarItems(role)}
       />
     </Sider>
   );
